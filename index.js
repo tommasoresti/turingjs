1b061b22f871b05bfd21183773c532b4141d52d8
Tommaso Resti <me@tommasoresti.it>
2020-11-13 02:33:31 +0100

    Stateless

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ fs = require('fs')
 let args = process.argv.slice(2);
 
 const content = fs.readFileSync(args[0], 'utf8');
-const stream = ['#', ...fs.readFileSync(args[1], 'utf8').split(""), '#'];
 const commands = content.split("\n");
 
-const toTree = (prevStates, line) => {
-    if(!line)
-        return undefined
+const movePosition = {
+    '>': n => n + 1,
+    '<': n => n - 1,
+    '_': n => n
+}
 
-    const firstLetter = line[0]
+const toTree = (prevStates, line) => {
+    if(line.length === 3)
+        return {
+            run: s => {
+                return ({
+                    stream: [...s.stream.slice(0, s.position), line[0], ...s.stream.slice(s.position + 1)],
+                    position: movePosition[line[1]](s.position),
+                    state: line[2]
+                });
+            }
+        }
+
     return {
         ...prevStates,
         ...{
-            [firstLetter]: {
-                ...prevStates[firstLetter],
+            [line[0]]: {
+                ...prevStates[line[0]],
                 ...toTree({}, line.slice(1))
             }
         }
@@ -26,25 +38,13 @@ const states = commands
     .map(line => line.split(" "))
     .reduce((prev, line) => toTree(prev, line[0]), {})
 
-let state = '0';
-let pointer = 1;
+let current = {
+    state: 0,
+    position: 1,
+    stream: ['#', ...fs.readFileSync(args[1], 'utf8').split(""), '#']
+}
 
-while(state !== '!') {
-    console.log(...stream)
-
-    const stateElement = states[state][stream[pointer]];
-    const writeElement = Object.keys(stateElement)[0]
-    const moveElement = Object.keys(Object.values(stateElement)[0])[0]
-    const jumpElement = Object.keys(Object.values(Object.values(stateElement)[0])[0])[0]
-
-    stream[pointer] = writeElement !== '_'
-        ? writeElement
-        : stream[pointer];
-
-    switch (moveElement) {
-        case '>': pointer++; break;
-        case '<': pointer--; break;
-    }
-
-    state = jumpElement
+while(current.state !== '!') {
+    console.log(...current.stream)
+    current = states[current.state][current.stream[current.position]].run(current)
 }
